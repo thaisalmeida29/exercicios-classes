e425fe37d81a413cf4098ed8da2f7220929c3445
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-05-25 20:30:37 -0300

    exercicio-aula-2505

---
+++ exercicioAula2505/Livro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioAula2505
+{
+    internal class Livro
+    {
+        /// <summary>
+        /// atributos
+        /// </summary>
+        public string titulo;
+        public string autor;
+        public int paginas;
+        public int ano;
+        public int edicao;
+
+        /// <summary>
+        /// metodo construtor
+        /// </summary>
+        /// <param name="tit"></param>
+        /// <param name="au"></param>
+        /// <param name="pag"></param>
+        /// <param name="an"></param>
+        /// <param name="ed"></param>
+        public Livro(string tit, string au, int pag, int an, int ed)
+        {
+            titulo = tit;
+            autor = au;
+            paginas = pag;
+            ano = an;
+            edicao = ed;
+        }
+
+        /// <summary>
+        /// metodos
+        /// </summary>
+        public void apresentarInfoLivro()
+        {
+            Console.WriteLine("O livro com titulo de " + titulo + " do autor " + autor + " edição " + edicao + " lançado no ano de " + ano + " com " + paginas + " páginas");
+        }
+
+    }
+}
---
+++ exercicioAula2505/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using exercicioAula2505;
+
+string t, a;
+int pag, ano, ed;
+
+Console.Write("Digite o titulo do livro: ");
+t = Console.ReadLine();
+Console.Write("Digite o autor do livro: ");
+a = Console.ReadLine();
+Console.Write("Digite a quantidade de paginas: ");
+pag = int.Parse(Console.ReadLine());
+Console.Write("Digite o ano: ");
+ano = int.Parse(Console.ReadLine());
+Console.Write("Digite a edição: ");
+ed = int.Parse(Console.ReadLine());
+Livro l = new Livro(t,a,pag,ano,ed);
+l.apresentarInfoLivro();
+
+

6a6b1444b1f907e49e262cd30dced33f7ffa73f8
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-05-25 18:59:55 -0300

    exercicio2

---
+++ exercicio2/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+     class Animal
+    {
+        /// <summary>
+        /// variavel que armazena o nome do animal
+        /// </summary>
+        
+      private string nome;
+        /// <summary>
+        /// variavel que armazena o tipo de animal
+        /// </summary>
+      private string tipo;
+
+        /// <summary>
+        /// construtor para um animal com todos os atributos e variaveis
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="tipo"></param>
+        public Animal (string nome, string tipo)
+        {
+            this.Nome = nome;
+            this.Tipo = tipo;
+        }
+
+        /// <summary>
+        /// propriedade de acesso ao nome
+        /// </summary>
+        public string Nome { get => nome; set => nome = value; }
+
+        /// <summary>
+        /// propriedade de acesso ao tipo
+        /// </summary>
+        public string Tipo { get => tipo; set => tipo = value; }
+
+
+    }
+}
---
+++ exercicio2/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Crie um programa em C# no qual o usuário deverá informar o nome e o tipo de cinco animais de estimação. 
+O programa deverá exibir na tela para o usuário quantos Cachorros, Gatos e Peixes foram informados.
+
+Regras que deverão ser seguidas para a implementação do programa:
+
+Os únicos tipos de animais válidos são Cachorro, Gato, Peixe.
+Caso seja informado um tipo diferente o programa repetir até o usuário digitar um tipo válido.
+É obrigatório criar uma classe para representar o Animal.
+A classe deverá possuir dois dados privados (propriedades) para representar as características do animal.
+A classe deverá possuir métodos de acesso (propriedade getter e setter) para permitir que o usuário armazene/leia os dados dos dois dados privados (propriedades).
+*/
+
+using exercicio2;
+string nome, tipo;
+int qntdCachorro = 0, qntdGato = 0, qntdPeixe = 0;
+
+
+List<Animal> listaAnimais = new List<Animal>();
+
+while (true)
+{
+
+
+    for (int i = 0; i <= 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine("Tipo do animal valido (Cachorro,Gato ou Peixe)");
+        Console.Write("Digite o nome do seu animal: ");
+        nome = Console.ReadLine();
+        Console.Write("Qual o tipo do seu animal? ");
+        tipo = Console.ReadLine();
+        listaAnimais.Add(new Animal(nome, tipo));
+
+        if (tipo == "Cachorro")
+        {
+            qntdCachorro++;
+        }
+        else if (tipo == "Gato")
+        {
+            qntdGato++;
+        }
+        else if (tipo == "Cachorro")
+        {
+            qntdPeixe++;
+        }
+    }
+}
+
+Console.WriteLine($"A quantidade de Cachorros é: {qntdCachorro}");
+Console.WriteLine($"A quantidade de Gatos é: {qntdGato}");
+Console.WriteLine($"A quantidade de Peixes é: {qntdPeixe}");
+

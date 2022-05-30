acc3f1f90a08ce4a9e2dd04c512f701b5b75727e
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-05-30 18:25:28 -0300

    desafio-garagem

---
+++ desafio-aula-26-05/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Trabalho para entender e aplicar a teoria de Orientação a Objetos (terceirização), uso adequado de Listas e Arquivos.
+Além disso, aplicar a ideia de menu de opções.
+
+Construir um programa em VS que gerencie veículos em garagem. O programa deve ter um menu de operação. Os dados dos
+veículos devem ser persistidos em arquivo e manipulados em uma lista dinâmica em memória.
+Os dados dos veículos são : string placa; DateTime dataHoraEntrada.
+
+MENU
+1 - Cadastrar veículo entrando na garagem
+2 - Exibir veículos cadastrados
+3 - Sair
+Opção: 
+
+Observações: Criar a classe Veiculo (placa, dataHoraEntrada); usar as classes List, StreamReader, StreamWriter, DateTime
+Use como base de dados o arquivo 'garagem.dat'.
+
+*/
+
+
+using desafioaula2605;
+
+List<Veiculo> listaCarros = new List<Veiculo>();
+string opcao;
+
+//popular a lista com dados do arquivo
+Util.carregarListas(listaCarros);
+
+do
+{
+    Console.Clear();
+
+    DateTime dataTeste = DateTime.Now;
+    Console.WriteLine(dataTeste.ToString());
+
+    Console.WriteLine("MENU");
+    Console.WriteLine("1 - Cadastrar veículos que entram na garagem");
+    Console.WriteLine("2 - Exibir veículos cadastrados");
+    Console.WriteLine("3 - Sair");
+
+    Console.Write("Opção: ");
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            //cadastrar um usuario;
+            Util.cadastrarVeiculo("Cadastrando veículo", listaCarros);
+            break;
+        case "2":
+            //listar usuarios;
+            Util.exibirLista("Exibindo a lista de carros", listaCarros);
+            break;
+        case "3":
+            Console.WriteLine("Obrigado por usar o sistema");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+    Console.Write("Pressione algo para continuar!");
+    Console.ReadKey();
+} while (opcao != "3");
---
+++ desafio-aula-26-05/Veiculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafioaula2605
+{
+    class Veiculo
+    {
+        string placa;
+        DateTime datahoraEntrada;
+
+        private void gerarDataHoraEntrada()
+        {
+            this.datahoraEntrada = DateTime.Now;
+            //this.datahoraEntrada = this.datahoraEntrada.ToString();
+        }
+
+        
+        public Veiculo(string placa)
+        {
+            this.placa = placa;
+            this.gerarDataHoraEntrada();
+        }
+
+       
+        public Veiculo(string placa, DateTime datahoraEntrada)
+        {
+            this.placa = placa;
+            this.datahoraEntrada = datahoraEntrada;
+        }
+
+        public string Placa { get => placa; set => placa = value; }
+
+        public DateTime DataHoraEntrada { get => datahoraEntrada; }
+    }
+}
